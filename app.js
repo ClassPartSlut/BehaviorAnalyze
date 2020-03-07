872e156e06dc8aa59dde10d0d953688821f2b801
MagicShow1999 <hw1635@nyu.edu>
2020-03-06 22:42:01 -0500

    add template

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'hbs');
+
+
 
 app.get('/', (req, res)=>{
 
     res.render('home',{});
 
-})
+});
 
 
 app.listen(3000);
